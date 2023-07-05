92d6a0928f8aa8a32707493dfe62afa41967d875
Renat Kislov <renatkaveev@gmail.com>
2023-07-05 22:48:24 +0500

    Initial Commit

---
+++ Dynamic island animation/Dynamic_island_animationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dynamic_island_animationApp.swift
+//  Dynamic island animation
+//
+//  Created by keet on 05.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct Dynamic_island_animationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
